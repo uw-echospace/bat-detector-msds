af74e13edcb284c9dc34ed3c172a57d37babf88b
Corbin Charpentier <corbin.charpentier@gmail.com>
2023-01-16 19:17:51 -0800

    wibble

--- scripts/audio_snipper.py
+++ scripts/audio_snipper.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python
 
 from pathlib import Path
-import argparse
-import pandas as pd
 
+import pandas as pd
+import type_enforced
 from scipy.io import wavfile
 
-# Gather our code in a main() function
-def main(args):
-    print(args)
 
-    # TODO call generateAllSnippets
-    # TODO convert all times to milliseconds
-    generateAllSnippets(
-            args['SourceAudioDir'], 
-            args['SourceTSVDir'], 
-            args['DestinationDir'], 
-            args['window'], 
-            args['tsv_extension'])
+@type_enforced.Enforcer
 
 def generateAllSnippets(srcDir: Path, tsvDir: Path, destDir: Path, windowSize: int, tsvExt: str):
     # Get all TSVs in destDir
@@ -26,7 +16,7 @@ def generateAllSnippets(srcDir: Path, tsvDir: Path, destDir: Path, windowSize: i
 
     for tsvInfo in tsvInfos:
         # Get the corresponding wav
-        wavPath = srcDir / tsvInfo['wavname'] + ".wav"
+        wavPath = srcDir / tsvInfo['wavname']
         generateSnippetsForTSV(wavPath, destDir, tsvInfo, windowSize)
 
 
@@ -36,7 +26,7 @@ def generateSnippetsForTSV(wavPath: Path, destDir: Path, tsvInfo: dict, windowSi
 
     for _, row in df.iterrows():
         # TSV HEADER: Selection	View	Channel	Begin Time (s)	End Time (s)	Low Freq (Hz)	High Freq (Hz)	Occupancy
-        start = row['Start Time (s)']
+        start = row['Begin Time (s)']
         end = row['End Time (s)']
 
         sample_rate, wav = wavfile.read(wavPath)
@@ -62,31 +52,3 @@ def singleSnippet(sample_rate, wav, start, end, windowSize):
     return wav[start:end]
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser( 
-        description = "Segment audio file into snippets that contain bat calls. The audio files are assumed to be in .wav format. The TSV files are assumed to be in the format output by the RavenPro.",
-    )
-
-    # TODO Specify your real parameters here.
-    parser.add_argument("SourceAudioDir", help = "The directory containing the audio files to segment.")
-    parser.add_argument("SourceTSVDir", help = "The directory containing the TSVs associated with each audio file.")
-    parser.add_argument("DestinationDir", help = "The directory to output the audio segments")
-
-    # TODO: default arguments
-    parser.add_argument(
-        "-w",
-        "--window",
-        help="The size of the window in milliseconds. If not present, use the exact intervals defined in the TSV file.",
-        action="store_true")
-
-    parser.add_argument(
-        "-t",
-        "--tsv_extension",
-        help="The expected file extension of the TSV files. Defaults to 'txt'.",
-        default="txt",
-        action="store_true")
-    
-    args = parser.parse_args()
-    
-    main(args)
-        
