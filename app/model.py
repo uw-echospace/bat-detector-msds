ab231818704cd4449c0d3a31542e5a551a325a63
Corbin Charpentier <corbin.charpentier@gmail.com>
2023-02-04 11:43:56 -0800

    rename src dir to app

---
+++ app/model.py
@@ -0,0 +1,7 @@
+
+
+
+def model_architecture():
+    """Define the model architecture"""
+    # TODO: integrate: https://towardsdatascience.com/how-to-cluster-images-based-on-visual-similarity-cd6e7209fe34
+    pass
