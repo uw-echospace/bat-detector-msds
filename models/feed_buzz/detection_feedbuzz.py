4e2aea10924659d71b0b61d929b7eb167c820d01
Ernesto Cediel <ecedielcc@gmail.com>
2023-03-06 00:44:23 -0800

    Added functions to run detection feedbuz and most helper functions

--- models/feed_buzz/detection_feedbuzz.py
+++ models/feed_buzz/detection_feedbuzz.py
@@ -4,13 +4,28 @@ import argparse
 import pandas as pd
 
 from models.detection_interface import DetectionInterface
+import models.feed_buzz.feed_buzz_helper as fbh
 
 class FeedBuzz(DetectionInterface):
-    def __init__(self):
-        pass
+    def __init__(self, peak_distance, peak_th, template_dict, num_matches_threshold, buzz_feed_range, alpha):
+        self.peak_distance = peak_distance
+        self.peak_th = peak_th
+        self.template_dict = template_dict
+        self.num_matches_threshold = num_matches_threshold
+        self.buzz_feed_range = buzz_feed_range
+        self.alpha = alpha
 
-    def run(self, sampleing_rate, audio) -> pd.DataFrame: # TODO: type annotations
-        pass
+    def run(self, audio_file) -> pd.DataFrame: # TODO: type annotations
+        rois_df = fbh.run_multiple_template_matching(
+                                            audio_file,
+                                            self.peak_distance,
+                                            self.peak_th,
+                                            self.template_dict,
+                                            self.num_matches_threshold, 
+                                            self.buzz_feed_range, 
+                                            self.alpha)
+        return rois_df
+        
 
     def get_name(self): # TODO: do we really need this? config passes in the name
         return "model_interface"
