e8a47efd4706ed06b4db80d65e62cc64806ec03e
Aditya Krishna <adkris1002@gmail.com>
2023-06-05 17:40:28 -0700

    made working .sh script for recover folder and 1 sd unit

--- scripts/pipeline_for_recovered_deployments.sh
+++ scripts/pipeline_for_recovered_deployments.sh
@@ -1,12 +1,18 @@
 #!/bin/zsh
 
 # Ask the user for the file name and search term
-read -p "Enter recover folder name: " recover_folder_path
+read -p "Enter recover folder path: " recover_folder_path
+read -p "Enter SD card label: " sd_unit
 
-recover_folder = $(basename -- recover_folder_path)
+recover_folder=$(basename "$recover_folder_path")
+echo $recover_folder
 
+. /home/adkris/miniconda3/etc/profile.d/conda.sh
 conda activate bat_msds
 
-for sd_unit in $recover_folder_path/*/ ; do
-    python src/batdt2_pipeline.py $recover_folder_path/$sd_unit batdetect2_pipeline__$recover_folder_$sd_unit.csv output_dir/$recover_folder/$sd_unit output/tmp
-done
+python src/batdt2_pipeline.py $recover_folder_path/$sd_unit batdetect2_pipeline__$recover_folder_$sd_unit.csv output_dir/$recover_folder/$sd_unit output/tmp
+
+# python src/batdt2_pipeline.py $recover_folder_path/$sd_unit2 batdetect2_pipeline__$recover_folder_$sd_unit2.csv output_dir/$recover_folder/$sd_unit2 output/tmp
+
+# python src/batdt2_pipeline.py $recover_folder_path/$sd_unit3 batdetect2_pipeline__$recover_folder_$sd_unit3.csv output_dir/$recover_folder/$sd_unit3 output/tmp
+
