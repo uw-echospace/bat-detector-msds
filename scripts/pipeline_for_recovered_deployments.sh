34aa85217f750f463b0cd790cc8e487e0b7e62c7
Aditya Krishna <adkris1002@gmail.com>
2023-06-06 10:37:25 -0700

    generated 2023-06-05 detections using batdetect2 pipeline

--- scripts/pipeline_for_recovered_deployments.sh
+++ scripts/pipeline_for_recovered_deployments.sh
@@ -1,15 +1,19 @@
 #!/bin/zsh
 
 # Ask the user for the file name and search term
-read -p "Enter recover folder path: " recover_folder_path
-read -p "Enter SD card label: " sd_unit
+recoverPath=$1
+sdUnit=$2
 
-recover_folder=$(basename "$recover_folder_path")
+recoverFolder=$(basename "$recoverPath")
+
+echo "Input Directory: $recoverFolder"/"$sdUnit";
+echo "Output CSV name: batdetect2_pipeline__"$recoverFolder"_"$sdUnit".csv"
+echo "Output Directory: output_dir/"$recoverFolder"/"$sdUnit""
 
 . /home/adkris/miniconda3/etc/profile.d/conda.sh
 conda activate bat_msds
 
-python src/batdt2_pipeline.py $recover_folder_path/$sd_unit batdetect2_pipeline__$recover_folder_$sd_unit.csv output_dir/$recover_folder/$sd_unit output/tmp
+python src/batdt2_pipeline.py "$recoverPath"/"$sdUnit" batdetect2_pipeline__"$recoverFolder"_"$sdUnit".csv output_dir/"$recoverFolder"/"$sdUnit" output/tmp
 
 # python src/batdt2_pipeline.py $recover_folder_path/$sd_unit2 batdetect2_pipeline__$recover_folder_$sd_unit2.csv output_dir/$recover_folder/$sd_unit2 output/tmp
 
