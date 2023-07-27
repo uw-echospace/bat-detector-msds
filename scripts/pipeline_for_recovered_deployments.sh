c091071b0542e6dd1f0356e80215562189d67ff1
Aditya Krishna <adkris1002@gmail.com>
2023-07-26 19:57:20 -0700

    updated sh pipeline commands

--- scripts/pipeline_for_recovered_deployments.sh
+++ scripts/pipeline_for_recovered_deployments.sh
@@ -5,6 +5,8 @@ recoverPath=$1
 sdUnit=$2
 runModel=$3
 genFig=$4
+shouldCSV=$5
+numProcesses=$6
 
 recoverFolder=$(basename "$recoverPath")
 
@@ -15,4 +17,4 @@ echo "Output Directory: output_dir/"$recoverFolder"/"$sdUnit""
 . /home/adkris/miniconda3/etc/profile.d/conda.sh
 conda activate bat_msds
 
-python3 src/batdt2_pipeline.py "$recoverPath"/"$sdUnit" bd2__"$recoverFolder"_"$sdUnit".csv output_dir/"$recoverFolder" output/tmp $runModel $genFig
+python3 src/batdt2_pipeline.py "$recoverPath"/"$sdUnit" bd2__"$recoverFolder"_"$sdUnit".csv output_dir/"$recoverFolder" output/tmp $runModel $genFig $shouldCSV $numProcesses
