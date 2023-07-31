ccb3d2fd610a385ec90a933f82c8f2f49529dbc9
Aditya Krishna <adkris1002@gmail.com>
2023-07-30 17:40:03 -0700

    fixing typo

--- src/batdt2_pipeline.py
+++ src/batdt2_pipeline.py
@@ -453,6 +453,7 @@ def construct_activity_grid(csv_name, ref_audio_files, good_audio_files, output_
 
     dets = pd.read_csv(f'{output_dir}/{csv_name}.csv')
     dets_per_file = dets.groupby(['input_file'])['input_file'].count()
+    print(dets_per_file)
 
     activity = []
     for ref_file in ref_audio_files:
