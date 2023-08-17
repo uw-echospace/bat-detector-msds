811ab9597c5008faa860d8edd7b61bf51d930282
Aditya Krishna <adkris1002@gmail.com>
2023-08-17 14:44:38 -0700

    updated script

--- scripts/pipeline_for_recovered_deployments.sh
+++ scripts/pipeline_for_recovered_deployments.sh
@@ -1,20 +1,18 @@
 #!/bin/zsh
 
 # Ask the user for the file name and search term
-recoverPath=$1
+recoverFolder=$1
 sdUnit=$2
 runModel=$3
 genFig=$4
 shouldCSV=$5
 numProcesses=$6
 
-recoverFolder=$(basename "$recoverPath")
-
-echo "Input Directory: $recoverPath"/"$sdUnit";
+echo "Input Directory: $recoverFolder"/"$sdUnit";
 echo "Output CSV name: bd2__"$recoverFolder"_"$sdUnit""
 echo "Output Directory: output_dir/"$recoverFolder"/"$sdUnit""
 
 . /home/adkris/miniconda3/etc/profile.d/conda.sh
 conda activate bat_msds
 
-python3 src/batdt2_pipeline.py "$recoverPath"/"$sdUnit" bd2__"$recoverFolder"_"$sdUnit" output_dir/"$recoverFolder" output/tmp $runModel $genFig $shouldCSV $numProcesses
+python3 src/batdt2_pipeline.py "$recoverFolder" "$sdUnit" output_dir/"$recoverFolder" output/tmp $runModel $genFig $shouldCSV $numProcesses
